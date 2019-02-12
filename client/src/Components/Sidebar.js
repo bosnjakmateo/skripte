02e133d94ce2b77eefb9ff5457fe67b4d2ea8aaf
Alen <alkuca@gmail.com>
2019-02-12 18:13:15 +0100

    mobile sidebar links fix

--- client/src/Components/Sidebar.js
+++ client/src/Components/Sidebar.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import '../App.css';
-import { Link } from 'react-router-dom';
+import {Link, withRouter} from 'react-router-dom';
+import {connect} from "react-redux";
+import {logoutUser} from "../Actions/authActions";
 
 class Sidebar extends Component {
     constructor(props){
         super(props);
         this.state = {};
+
+        this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    }
+
+    handleLogoutClick(e){
+        e.preventDefault();
+        this.props.logoutUser();
+        this.props.history.push("/login")
     }
 
     render() {
@@ -13,9 +23,9 @@ class Sidebar extends Component {
             <div className="sidebar">
                 <div className="sidebar-right">
                     <Link to="/home" className="sidebar-right-links">Moji Kolegiji</Link>
-                    <Link to="/svikolegiji" className="sidebar-right-links">Svi Kolegiji</Link>
-                    <Link to="/upload" className="sidebar-right-links">Upload</Link>
-                    <p className="sidebar-right-links">Odjavi se</p>
+                    <Link to="/institucije" className="sidebar-right-links">Pronađi Kolegij</Link>
+                    <Link to="/account" className="sidebar-right-links">Account</Link>
+                    <p onClick={this.handleLogoutClick} className="sidebar-right-links">Odjavi se</p>
                 </div>
                 <div onClick={this.props.clicker.bind(this)} className="sidebar-left">
                 </div>
@@ -24,4 +34,10 @@ class Sidebar extends Component {
     }
 }
 
-export default Sidebar;
+const mapStateToProps = (state) => ({
+    auth: state.auth
+});
+
+export default withRouter(connect(mapStateToProps, { logoutUser })(Sidebar))
+
+
