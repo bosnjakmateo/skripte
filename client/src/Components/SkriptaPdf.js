512e1d2a849fd3ab6753a0b6e3dd501feba1085f
Alen <alkuca@gmail.com>
2019-03-12 13:14:26 +0100

    revert script pdf page to show sample file

--- client/src/Components/SkriptaPdf.js
+++ client/src/Components/SkriptaPdf.js
@@ -1,33 +1,16 @@
 import React, { Component } from 'react';
 import '../App.css';
-import { Document, Page } from 'react-pdf';
-
 
 class SkriptaPdf extends Component {
     constructor(props){
         super(props);
-        this.state = {
-            numPages: null,
-            pageNumber: 1,
-        };
-        this.onDocumentLoadSuccess = this.onDocumentLoadSuccess.bind(this);
-    }
-
-    onDocumentLoadSuccess = ({ numPages }) => {
-        this.setState({ numPages });
+        this.state = {};
     }
 
     render() {
-        const { pageNumber, numPages } = this.state;
         return (
             <div className="pdf-container">
-                <Document
-                    file="somefile.pdf"
-                    onLoadSuccess={this.onDocumentLoadSuccess}
-                >
-                    <Page pageNumber={pageNumber} />
-                </Document>
-                <p>Page {pageNumber} of {numPages}</p>
+                <embed id="asd" src="http://www.pdf995.com/samples/pdf.pdf#view=FitH"/>
             </div>
         );
     }
